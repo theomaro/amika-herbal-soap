5e528467f5051fa85b80374560ecd0322433dd1b
Mathew Maro <149648717+theomaro@users.noreply.github.com>
2024-06-16 10:26:46 +0300

    add image slider

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,20 @@
 "use strict";
-const sayHello = (name) => {
-    console.log("Hello " + name);
-};
-sayHello("World");
+const btns = document.querySelectorAll(".btn");
+const images = document.querySelectorAll(".image");
+let count = 1;
+const start = 1, end = images.length;
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (count >= start + 1 && btn.className.includes("prev")) {
+            count--;
+        }
+        if (count <= end - 1 && btn.className.includes("next")) {
+            count++;
+        }
+        images.forEach((image) => {
+            image.classList.add("hidden");
+        });
+        images[count - 1].classList.remove("hidden");
+    });
+});
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,26 @@
-const sayHello = (name: string): void => {
-    console.log("Hello " + name);
-}
+const btns = document.querySelectorAll(".btn") as NodeListOf<HTMLButtonElement>;
+const images = document.querySelectorAll(
+  ".image"
+) as NodeListOf<HTMLImageElement>;
 
-sayHello("World")
+let count = 1;
+const start = 1,
+  end = images.length;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (count >= start + 1 && btn.className.includes("prev")) {
+      count--;
+    }
+
+    if (count <= end - 1 && btn.className.includes("next")) {
+      count++;
+    }
+
+    images.forEach((image) => {
+      image.classList.add("hidden");
+    });
+
+    images[count - 1].classList.remove("hidden");
+  });
+});
